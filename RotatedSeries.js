c86ed385b6996cd14649171a95e254ea8704bdad
ricchard117 <60483677+ricchard117@users.noreply.github.com>
2020-02-04 12:04:41 -0600

    Add files via upload

---
+++ RotatedSeries.js
@@ -0,0 +1,51 @@
+(function() {
+	let template = document.createElement("template");
+template.innerHTML = `
+<style>
+#chartdiv {
+  width: 100%;
+  height: 500px;
+}
+</style>
+
+<!-- Resources -->
+<script src="https://www.amcharts.com/lib/4/core.js"></script>
+<script src="https://www.amcharts.com/lib/4/charts.js"></script>
+<script src="https://www.amcharts.com/lib/4/themes/animated.js"></script>
+
+	
+`;
+class RotatedSeries extends HTMLElement {
+
+constructor() {
+super();
+
+);
+this._props = {};
+}
+onCustomWidgetBeforeUpdate(changedProperties) {
+this._props = { ...this._props, ...changedProperties };
+}
+onCustomWidgetAfterUpdate(changedProperties) {
+if ("color" in changedProperties) {
+this.style["background-color"] = changedProperties["color"];
+}
+if ("opacity" in changedProperties) {
+this.style["opacity"] = changedProperties["opacity"];
+}
+}
+}
+customElements.define("com_sap_sample_RotatedSeries", RotatedSeries);
+})();
+
+
+
+
+
+
+
+
+
+
+
+})();	
